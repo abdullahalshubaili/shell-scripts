9f65078a6fa433dd3751b63404c66f1775d8364b
root <root@localhost.localdomain>
2016-12-14 02:32:44 -0500

    for searching in whois

--- findip.sh
+++ findip.sh
@@ -8,5 +8,13 @@ grep -v [a-z] ipd.txt | mawk -F "." '{print $1,$2,$3,$4}' | sed -e 's/\s/./g' > 
 
 sort ips2.txt | uniq -d > ips3.txt
 sort ipd2.txt | uniq -d > ipd3.txt
+rm ips.txt ips2.txt ipd.txt ipd2.txt
+cat ips3.txt ipd3.txt > allip2.txt
+sort allip2.txt | uniq -d > allip.txt
+rm ips3.txt ipd3.txt allip2.txt
+./whois.sh
+#need to put orginaztion name
+#ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | awk '{print $2}')"
+#ip2="$(echo mawk $ip -F "." '{print $1,$2,$3}' >$ip | grep -v $ip allip.txt >new.txt) > IP.txt"
 
-
+#nmap -iL allip.txt --script=whois-ip.nse | grep 'for\|country' >> IPfindings.txt
